d22856f0a9783af739fe8207babde6791b7697e0
allmichel <allan.michel.info18@gmail.com>
2023-05-22 19:35:39 -0300

    4_iterando_listas_explicacao

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,37 +1,30 @@
-//let idElementoAudio = 
 function tocaSom(idElementoAudio){
     document.querySelector(idElementoAudio).play();
 }
-
-function tocaSomClap(){
-    document.querySelector('#som_tecla_clap').play();
-            
-    }
-  /* O uso do ponto(.) faz com que entre no elemento que está sendo buscado pelo querySelector.
-  e a partir dele, o js vai mostrar uma série de opções(funcionalidade e propriedades) que serão 
-  possíveis ser consultadas ou executadas 
-  
-  OBS: em  <- onclick = tocaSomPom() -> o js não atribui o <- tocaSomPom ->, mas 
-  ele tenta executar automaticamente a função ao a pagina ser carregada. 
-  Mas para o <- onclick -> atribuir esta função, ele precisa estar sem os parenteses.   
-  */
-//document.querySelector('.tecla_pom').onclick = tocaSomPom;
-
 // Referencias => <- const ListaDeTeclas ->
 const ListaDeTeclas =  document.querySelectorAll('.tecla');
+// criada a variavel contadora
 let i = 0;
 
+// o <- length -> conta o numeros de elementos da lista. Logo, ao invés de usar <- (i<9) -> usa-se o length
 while (i<ListaDeTeclas.length) {
-    
+
+    // a <- tecla -> está atribuindo os elementos da <- ListaDeTeclas ->
     const tecla = ListaDeTeclas[i];
     
+    // clasList lista as classes. Ex:
+    // ListaDeTeclas[0].classlist
+    // resultado = DOMTokenList [ "tecla", "tecla_pom" ]
+ 
+    //1 pq ele vai sempre pegar o elemento 1 da classe(ex: tecla_pom)
     const instrumento = tecla.classList[1];
     //template string
-    //obs: procurar por exercicios com template string
+    //Ele vai concatenar o id <- #som-> com por exemplo, a <- tecla_pom ->
     const idAudio = `#som_${instrumento}`;
-
+    
     console.log(idAudio);
     //                       Função anônima
+
     ListaDeTeclas[i].onclick = function(){
         tocaSom(idAudio);
     };
